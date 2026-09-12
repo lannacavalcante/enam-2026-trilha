c61b85d47682c1412483285ddcefa2ab1c76db07
lannacavalcante <lannacavalcast@gmail.com>
2026-09-12 16:17:11 -0300

    Create dados.js

---
+++ casas/dados.js
@@ -0,0 +1,11 @@
+// casas/dados.js
+// Aqui você centraliza todas as casas criadas para a trilha do ENAM 2026.
+// Sempre que criar um novo arquivo casaX.js, basta registrá-lo nesta lista.
+
+const trilhaENAM = [
+  casa1,
+  casa2,
+  // Quando criar a casa 3, basta adicionar aqui:
+  // casa3,
+  // casa4,
+];
